e5e9e2861761be5185563630f45d6ae5dcdcf7ea
Subifa Maharjan <83801961+Subifa071@users.noreply.github.com>
2021-09-22 18:52:38 +0545

    testing

--- hangman.py
+++ hangman.py
@@ -2,66 +2,87 @@ from tkinter import *
 from tkinter import messagebox
 from string import ascii_uppercase
 import random
+import pygame
+from pygame import mixer
 
 window = Tk()
 window.title("Hangman")
+window.resizable(width=False, height=False)
+pygame.mixer.pre_init(3400, -16, 1, 512)
+mixer.init()
+pygame.init()
 
-word_list = ["RUSSIA", "ARGENTINA", "MEXICO", "COLOMBIA", "KAZAKHSTAN", "GERMANY", "TURKEY", "HUNGARY", "FINLAND",
-             "INDIA", "CHINA", "SWEDEN",
-             "MONGOLIA", "MOROCCO", "NORWAY", "CHILE", "IRELAND", "ICELAND", "CROATIA", "ESTONIA", "SPAIN", "NIGERIA",
-             "JAPAN", "UKRAINE", "ALGERIA"]
+#sounds
+win_fx = pygame.mixer.Sound("win.wav")
+win_fx.set_volume(50)
+
+lose_fx = pygame.mixer.Sound("lose.wav")
+lose_fx.set_volume(50)
+
+
+word_list = []
+with open("words.txt") as f:
+    for line in f:
+        word_list.append(line.strip())
+
 
 photos = (PhotoImage(file="E:/images/hang0.png"), PhotoImage(file="E:/images/hang1.png"), PhotoImage(file="E:/images/hang2.png"),
-          PhotoImage(file="E:/images/hang3.png"),
-          PhotoImage(file="E:/images/hang4.png"), PhotoImage(file="E:/images/hang5.png"), PhotoImage(file="E:/images/hang6.png"),
-          PhotoImage(file="E:/images/hang7.png"),
-          PhotoImage(file="E:/images/hang8.png"), PhotoImage(file="E:/images/hang9.png"),
-          PhotoImage(file="E:/images/hang10.png"), PhotoImage(file="E:/images/hang11.png"))
+PhotoImage(file="E:/images/hang3.png"),
+PhotoImage(file="E:/images/hang4.png"), PhotoImage(file="E:/images/hang5.png"), PhotoImage(file="E:/images/hang6.png"),
+PhotoImage(file="E:/images/hang7.png"),
+PhotoImage(file="E:/images/hang8.png"), PhotoImage(file="E:/images/hang9.png"),
+PhotoImage(file="E:/images/hang10.png"), PhotoImage(file="E:/images/hang11.png"))
 
 
 def newGame():
     global the_word_withSpaces
     global numberOfGuesses
-    numberOfGuesses = 0
-    imgLabel.config(image=photos[0])
+    numberOfGuesses = 0  # intialize
+    imgLabel.config(image=photos[0])  # config = display images
     the_word = random.choice(word_list)
-    the_word_withSpaces = " ".join(the_word)
-    lblWord.set(" ".join("_" * len(the_word)))
+    the_word_withSpaces = " ".join(the_word)  # or else wont work
+    lblWord.set(" ".join("_" * len(the_word)))  # for dash lines
 
 
 def guess(letter):
     global numberOfGuesses
     if numberOfGuesses < 11:
         txt = list(the_word_withSpaces)
-        guessed = list(lblWord.get())
+        guessed = list(lblWord.get())  # if correct letter print on top of dash
         if the_word_withSpaces.count(letter) > 0:
-            for c in range(len(txt)):
+            for c in range(
+                    len(txt)):  # wont print letter on top of dash, remains blank if the guessed letter is true & if not hangman is drawn
                 if txt[c] == letter:
                     guessed[c] = letter
             lblWord.set("".join(guessed))
             if lblWord.get() == the_word_withSpaces:
+                win_fx.play()
                 messagebox.showinfo("Hangman", "You guessed it!")
                 newGame()
+
         else:
             numberOfGuesses += 1
             imgLabel.config(image=photos[numberOfGuesses])
             if numberOfGuesses == 11:
+                lose_fx.play()
                 messagebox.showwarning("Hangman", "Game Over")
 
 
+# GUI
 imgLabel = Label(window)
 imgLabel.grid(row=0, column=0, columnspan=3, padx="10", pady="40")
 imgLabel.config(image=photos[0])
 lblWord = StringVar()
-Label(window, textvariable=lblWord, font=("Consolas 24 bold")).grid(row=0, column=3, columnspan=6, padx="10")
+Label(window,fg="green", textvariable=lblWord, font=("Consolas 24 bold")).grid(row=0, column=3, columnspan=6, padx="10")
 
+# goes through all 26 alphabets
 n = 0
 for c in ascii_uppercase:
-    Button(window, text=c, command=lambda c=c: guess(c), font=("Helvetica 18"), width=4).grid(row=1 + n // 9,
+    Button(window,bg="bisque", text=c, command=lambda c=c: guess(c), font=("Helvetica 18"), width=4).grid(row=1 + n // 9,
                                                                                               column=n % 9)
     n += 1
 
-Button(window, text="New\nGame", command=lambda: newGame(), font=("Helvetica 10 bold")).grid(row=3, column=8,
+Button(window,bg="rosybrown",text="New\nGame", command=lambda: newGame(), font=("Helvetica 10 bold")).grid(row=3, column=8,
                                                                                              sticky="NSWE")
 
 newGame()
