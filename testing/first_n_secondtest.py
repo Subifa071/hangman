da5af7419b9b220c2b6e437692a502c23d54c64d
Subifa Maharjan <83801961+Subifa071@users.noreply.github.com>
2021-10-02 21:12:16 +0545

    first and second testing

--- testing/first_n_secondtest.py
+++ testing/first_n_secondtest.py
@@ -1,8 +1,18 @@
 """
-This tests random by using pytest.
+This tests name and contact number by using pytest.
 """
 import pytest
+@pytest.fixture
+def tester():
+    name = "Jack"
+    contact = 9803452929
+    return [name, contact]
 
-@pytest.mark.parametrize("random_word, guess_word", [("titans", "titans"), ("titans", "willow")])
-def test_method(random_word, guess_word):
-    assert random_word == guess_word
+def testing_1(tester):
+    first_name = "Jack"
+    assert tester[0] == first_name
+
+def testing_2(tester):
+
+    contact_num = 9003452929
+    assert tester[1] == contact_num
